6eb7b2af5b00df0443f8bbe5be9e22907c2dc57d
yotamlevit <43199124+yotamlevit@users.noreply.github.com>
2018-10-23 23:14:53 +0300

    GUI_23/10/2018

--- test.py
+++ test.py
@@ -1,83 +1,92 @@
-# Import a library of functions called 'pygame'
-import pygame
-from math import pi
- 
-# Initialize the game engine
-pygame.init()
- 
-# Define the colors we will use in RGB format
-BLACK = (  0,   0,   0)
-WHITE = (255, 255, 255)
-BLUE =  (  0,   0, 255)
-GREEN = (  0, 255,   0)
-RED =   (255,   0,   0)
- 
-# Set the height and width of the screen
-size = [400, 300]
-screen = pygame.display.set_mode(size)
- 
-pygame.display.set_caption("Example code for the draw module")
- 
-#Loop until the user clicks the close button.
-done = False
-clock = pygame.time.Clock()
- 
-while not done:
- 
-    # This limits the while loop to a max of 10 times per second.
-    # Leave this out and we will use all CPU we can.
-    clock.tick(10)
-     
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done=True # Flag that we are done so we exit this loop
- 
-    # All drawing code happens after the for loop and but
-    # inside the main while done==False loop.
-     
-    # Clear the screen and set the screen background
-    screen.fill(WHITE)
- 
-    # Draw on the screen a GREEN line from (0,0) to (50.75) 
-    # 5 pixels wide.
-    pygame.draw.line(screen, GREEN, [0, 0], [50,30], 5)
- 
-    # Draw on the screen a GREEN line from (0,0) to (50.75) 
-    # 5 pixels wide.
-    pygame.draw.lines(screen, BLACK, False, [[0, 80], [50, 90], [200, 80], [220, 30]], 5)
-    
-    # Draw on the screen a GREEN line from (0,0) to (50.75) 
-    # 5 pixels wide.
-    pygame.draw.aaline(screen, GREEN, [0, 50],[50, 80], True)
+"""
+author: Yotam Levir
+project: Holyday project
+"""
 
-    # Draw a rectangle outline
-    pygame.draw.rect(screen, BLACK, [75, 10, 50, 20], 2)
-     
-    # Draw a solid rectangle
-    pygame.draw.rect(screen, BLACK, [150, 10, 50, 20])
-     
-    # Draw an ellipse outline, using a rectangle as the outside boundaries
-    pygame.draw.ellipse(screen, RED, [225, 10, 50, 20], 2) 
+from Tkinter import *
+from Right_Triangle import *
+from Isosceles_tri import *
+from Square import *
+from Equilateral_Triangle import *
+from Rectangle import *
+from Parallelogram import *
+from Circle import *
+from Ellipse import *
+from Main_GUI import New_Toplevel
 
-    # Draw an solid ellipse, using a rectangle as the outside boundaries
-    pygame.draw.lelipse(screen, RED, [300, 10, 50, 20])
- 
-    # This draws a triangle using the polygon command
-    pygame.draw.polygon(screen, BLACK, [[100, 100], [0, 200], [200, 200]], 5)
-  
-    # Draw an arc as part of an ellipse. 
-    # Use radians to determine what angle to draw.
-    pygame.draw.arc(screen, BLACK,[210, 75, 150, 125], 0, pi/2, 2)
-    pygame.draw.arc(screen, GREEN,[210, 75, 150, 125], pi/2, pi, 2)
-    pygame.draw.arc(screen, BLUE, [210, 75, 150, 125], pi,3*pi/2, 2)
-    pygame.draw.arc(screen, RED,  [210, 75, 150, 125], 3*pi/2, 2*pi, 2)
-    
-    # Draw a circle
-    pygame.draw.circle(screen, BLUE, [60, 250], 40)
-    
-    # Go ahead and update the screen with what we've drawn.
-    # This MUST happen after all the other drawing commands.
-    pygame.display.flip()
- 
-# Be IDLE friendly
-pygame.quit()
+g = New_Toplevel()
+
+
+
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 1000
+BLACK = "black"
+WHITE = "white"
+BLUE = "blue"
+GREEN = "green"
+RED = "red"
+
+#Init scree
+root = Tk()
+canvas = Canvas(root, width=1000, height=1000)
+canvas.pack()
+
+
+def main():
+
+    #polygon
+    dot = [100, 100, 150, 200, 300, 400, 500, 500]
+    poly = Polygon(dot, WHITE)
+    poly.draw_me(canvas)
+
+    #right triangle
+    val_ri_tri = [500, 100, 100, 300]
+    ri_tri = Right_Tri(val_ri_tri, BLUE)
+    ri_tri.draw_me(canvas)
+    print "The right triangle's area is: ", ri_tri.calc_area()
+
+    #Isosceles triangle
+    is_tri1 = [500, 500, 600, 500]
+    is_tri = Isosceles_Tri(is_tri1, BLACK)
+    is_tri.draw_me(canvas)
+    print "The Isosceles triangle's area is:  ", is_tri.calc_area()
+
+    #Equilateral Triangle
+    equ = [500, 500, 300]
+    equ_tri = Equilateral_Triangle(equ, GREEN)
+    equ_tri.draw_me(canvas)
+    print "The Equilateral Triangle's area is: ", equ_tri.calc_area()
+
+    #square
+    sq = [500, 500, 150]
+    sqr = Square(sq, RED)
+    sqr.draw_me(canvas)
+    print "The square's area is: ", sqr.calc_area()
+
+    #Rectangle
+    re = [300, 300, 150, 50]
+    rec = Rectangle(re, GREEN)
+    rec.draw_me(canvas)
+    print "The Rectangle's area is: ", rec.calc_area()
+
+    #Parallelogram
+    pa = [500, 400, 100, 50, 150]
+    para = Parallelogram(pa, RED)
+    para.draw_me(canvas)
+    print "The Parallelogram area is: ", para.calc_area()
+
+    #Circle
+    cer = [200, 200, 200]
+    cr = Circle(cer, RED)
+    cr.draw_me(canvas)
+    print "The Circle area is: ", cr.calc_area()
+
+    #Ellipse
+    ell = [200, 200, 200, 100]
+    el = Ellipse(ell, BLUE)
+    el.draw_me(canvas)
+    print "The Ellipse area is: ", el.calc_area()
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
